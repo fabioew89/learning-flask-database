1c844f3006869e61a57a74311c1e6bc3c1931982
Fábio Ewerton <fabioew89@gmail.com>
2024-10-17 16:43:00 -0400

    jumbotron

--- app/controllers/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__main__)
-
-from app.controllers import routes
--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+from app import app
 
-@app.route('/')
-def method_name():
-    return 'app'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/outro', methods=['GET', 'POST'])
+def outro():
+    return render_template('outro.html')
