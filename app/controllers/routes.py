74dee8965a9e4d3e7b06194415c47f87265dec48
Fábio Ewerton <fabioew89@gmail.com>
2024-10-18 17:52:57 -0400

    modified:   app/controllers/routes.py

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -8,8 +8,8 @@ def index():
 @app.route('/new-user', methods=['GET', 'POST'])
 def user_create():
     if request.method == 'POST':
-        if not request.form['f_username'] or not request.form['f_email']:
-            username = request.form['f_username']
-            email = request.form['f_email']
+        if not request.form['f-username'] or not request.form['f-email']:
+            username = request.form['f-username']
+            email = request.form['f-email']
             return render_template('user/create.html', username=username, email=email)
     return render_template('user/create.html')
