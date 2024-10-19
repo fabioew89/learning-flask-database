d83b34e767f05a1d456d7c46f25a59dfe18510e7
Fabio Ewerton <fabioew89@gmail.com>
2024-10-19 19:29:27 -0400

    modified:   app/models/model.py

--- app/models/model.py
+++ app/models/model.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class Users(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
-    username = Mapped[str] = mapped_column(unique=True, nullable=False)
-    email = Mapped[str] = mapped_column(unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
