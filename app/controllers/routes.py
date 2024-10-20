aa46dfccdad639e8a2f5f149b54ebb39038262ab
Fabio Ewerton <fabioew89@gmail.com>
2024-10-19 22:14:10 -0400

    modified:   app/controllers/routes.py

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash, redirect
+from flask import request, render_template, flash, redirect, url_for
 from app import app, db
 from app.models.model import Users
 
@@ -8,23 +8,28 @@ def index():
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
-    username = request.form.get('username')
-    email = request.form.get('email')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
 
-    print(f'username {username}, email: {email}')
+        #print variaveis username e email
+        print(f'username {username}, email: {email}')
 
-    if username and email:
-        new_user = Users(username=username, email=email)
+        if username and email:
+            new_user = Users(username=username, email=email)
 
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            flash(f'Usuario adicionado com sucesso', 'success')
-        except Exception as e:
-            db.session.rollback()
-            flash(f'Erro ao adicionar usuario {e}', 'error')
-        
-        return redirect('/')
-    else:
-        flash(f'preencha os campos', 'error')
-        return redirect('/')
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash(f'Usuario adicionado com sucesso', 'success')
+                return redirect(url_for('index'))
+
+            except Exception as e:
+                db.session.rollback()
+                flash(f'Erro ao adicionar usuario {e}', 'error')
+                return redirect(url_for('add_user'))
+        else:
+            flash(f'preencha os campos', 'error')
+            return redirect('add_user')
+    
+    return render_template('add_user.html')
